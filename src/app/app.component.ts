fffe7cd6f5995d5f6b0df4cf7c925a24a424fcf9
INTRANET\fjakovljevic <fjakovljevic@cs.hr>
2020-06-24 19:01:47 +0200

    Added menu and homepage.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'ubTask';
+    public menuItems: Array<MenuItem> = [
+        { name: 'Home', link: '' },
+        { name: 'Contact Us', link: '' }
+    ];
+    public activeItemName = 'Home';
+}
+
+interface MenuItem {
+    name: string;
+    link: string;
 }
